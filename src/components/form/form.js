f10cfaaeaefdcb1bd7b60687305c00cc09f3e5b5
ElvisLin125 <elvis.125.ma@gmail.com>
2021-01-30 18:07:30 -0800

    Updated Form layout - location, diagnosis

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './form.css'
 
-const Form = () => {
+function Form() {
     
 
     return (
@@ -10,10 +10,79 @@ const Form = () => {
             <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital@1&display=swap" rel="stylesheet"/>
             
             <form id="daily-form" className="d-flex flex-column">       
-                <h2>How Have You Been?</h2>
-                <input type="date" className="form-control" id="date"  required/>
-                <textarea type="text" className="form-control" id="symptoms" placeholder="Symptoms" required/>
-                <textarea type="text" className="form-control" id="notes" placeholder="Notes"/>
+                <h2 id="prompt">How Have You Been?</h2>
+                <div id="header-line"></div>
+                <div id="covid-div">
+                    <h4 id="covid-question">Do you have Covid?</h4>
+                    <div id="diagnosis-options">
+                        <input id="positive" type="radio" name="diagnosis" value="positive" checked />
+                        <label for="positive"><span></span>Yes</label >
+                        <input id="negative" type="radio" name="diagnosis" value="negative" />
+                        <label for="negative"><span></span>No</label>
+                    </div>
+                </div>
+                <input type="date" className="form-control" id="date" name="date" required/>
+                <div id="location">
+                    <input type="text"  className="form-control" id="city" name="city" placeholder="City"/>
+                    <select id="state" className="form-control" name="state" >
+
+                        <option value='AL'>Alabama</option>
+                        <option value='AK'>Alaska</option>
+                        <option value='AZ'>Arizona</option>
+                        <option value='AR'>Arkansas</option>
+                        <option value='CA' selected>California</option>
+                        <option value='CO'>Colorado</option>
+                        <option value='CT'>Connecticut</option>
+                        <option value='DE'>Delaware</option>
+                        <option value='DC'>District of Columbia</option>
+                        <option value='FL'>Florida</option>
+                        <option value='GA'>Georgia</option>
+                        <option value='HI'>Hawaii</option>
+                        <option value='ID'>Idaho</option>
+                        <option value='IL'>Illinois</option>
+                        <option value='IN'>Indiana</option>
+                        <option value='IA'>Iowa</option>
+                        <option value='KS'>Kansas</option>
+                        <option value='KY'>Kentucky</option>
+                        <option value='LA'>Louisiana</option>
+                        <option value='ME'>Maine</option>
+                        <option value='MD'>Maryland</option>
+                        <option value='MA'>Massachusetts</option>
+                        <option value='MI'>Michigan</option>
+                        <option value='MN'>Minnesota</option>
+                        <option value='MS'>Mississippi</option>
+                        <option value='MO'>Missouri</option>
+                        <option value='MT'>Montana</option>
+                        <option value='NE'>Nebraska</option>
+                        <option value='NV'>Nevada</option>
+                        <option value='NH'>New Hampshire</option>
+                        <option value='NJ'>New Jersey</option>
+                        <option value='NM'>New Mexico</option>
+                        <option value='NY'>New York</option>
+                        <option value='NC'>North Carolina</option>
+                        <option value='ND'>North Dakota</option>
+                        <option value='OH'>Ohio</option>
+                        <option value='OK'>Oklahoma</option>
+                        <option value='OR'>Oregon</option>
+                        <option value='PA'>Pennsylvania</option>
+                        <option value='RI'>Rhode Island</option>
+                        <option value='SC'>South Carolina</option>
+                        <option value='SD'>South Dakota</option>
+                        <option value='TN'>Tennessee</option>
+                        <option value='TX'>Texas</option>
+                        <option value='UT'>Utah</option>
+                        <option value='VT'>Vermont</option>
+                        <option value='VA'>Virginia</option>
+                        <option value='WA'>Washington</option>
+                        <option value='WV'>West Virginia</option>
+                        <option value='WI'>Wisconsin</option>
+                        <option value='WY'>Wyoming</option>
+                    </select>
+                </div>
+                <textarea type="text" className="form-control" id="symptoms" name="symptoms"
+                    placeholder="How did you feel? Did you experience any symptoms?" required
+                />
+                <textarea type="text" className="form-control" id="notes" name="notes" placeholder="Notes"/>
                 <input type="submit" className="btn btn-primary" id="submitButton" value="Submit"/>   
             </form>
         </div>
