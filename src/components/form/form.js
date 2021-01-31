938bcd6967cc4a272616c7153b71dba3e5706259
Jane <vo.janehanh@gmail.com>
2021-01-30 19:02:42 -0800

    Created useState() hooks for form and made form go back to default after submission

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './form.css'
 
 function Form() {
-    
+    // TO DO: set up database to send these value on form submit to the db
+    const [positive, setPositive] = useState(true);
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('CA');
+    const [date, setDate] = useState('');
+    const [symptoms, setSymptoms] = useState('');
+    const [notes, setNotes] = useState('');
+
+    console.log('-------- FORM VALUES --------')
+    console.log("positive: ", positive);
+    console.log("city: ", city);
+    console.log("state: ", state);
+    console.log("date: ", date);
+    console.log("symptoms: ", symptoms);
+    console.log("notes: ", notes);
+    console.log("------------------------------")
+
+    const handleFormSubmit = () => {
+        // post into database
+        // at the end, clear the form
+        clearForm();
+    }
+
+    const clearForm = () => {
+        setPositive(true);
+        setCity('');
+        setState('');
+        setDate('');
+        setSymptoms('');
+        setNotes('');
+    }
 
     return (
         <div>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
             <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital@1&display=swap" rel="stylesheet"/>
             
-            <form id="daily-form" className="d-flex flex-column">       
+            <form id="daily-form" className="d-flex flex-column" onSubmit={handleFormSubmit}>       
                 <h2 id="prompt">How Have You Been?</h2>
                 <div id="header-line"></div>
                 <div id="covid-div">
                     <h4 id="covid-question">Do you have Covid?</h4>
                     <div id="diagnosis-options">
-                        <input id="positive" type="radio" name="diagnosis" value="positive" checked />
-                        <label for="positive"><span></span>Yes</label >
-                        <input id="negative" type="radio" name="diagnosis" value="negative" />
-                        <label for="negative"><span></span>No</label>
+                        <input id="positive" type="radio" name="diagnosis" value="positive" onClick={()=>setPositive(true)} defaultChecked />
+                        <label htmlFor="positive"><span></span>Yes</label >
+                        <input id="negative" type="radio" name="diagnosis" value="negative" onClick={()=>setPositive(false)}/>
+                        <label htmlFor="negative"><span></span>No</label>
                     </div>
                 </div>
-                <input type="date" className="form-control" id="date" name="date" required/>
+                <input type="date" className="form-control" id="date" name="date" onChange={(e) => setDate(e.target.value)}required/>
                 <div id="location">
-                    <input type="text"  className="form-control" id="city" name="city" placeholder="City"/>
-                    <select id="state" className="form-control" name="state" >
-
+                    <input type="text"  className="form-control" id="city" name="city" onChange={(e) => setCity(e.target.value)} placeholder="City" />
+                    <select id="state" className="form-control" name="state" onChange={(e) => setState(e.target.value)}>
                         <option value='AL'>Alabama</option>
                         <option value='AK'>Alaska</option>
                         <option value='AZ'>Arizona</option>
                         <option value='AR'>Arkansas</option>
-                        <option value='CA' selected>California</option>
+                        <option value='CA' defaultValue>California</option>
                         <option value='CO'>Colorado</option>
                         <option value='CT'>Connecticut</option>
                         <option value='DE'>Delaware</option>
@@ -80,9 +109,10 @@ function Form() {
                     </select>
                 </div>
                 <textarea type="text" className="form-control" id="symptoms" name="symptoms"
+                    onChange={(e)=>setSymptoms(e.target.value)}
                     placeholder="How did you feel? Did you experience any symptoms?" required
                 />
-                <textarea type="text" className="form-control" id="notes" name="notes" placeholder="Notes"/>
+                <textarea type="text" className="form-control" id="notes" name="notes" placeholder="Notes" onChange={(e) => setNotes(e.target.value)}/>
                 <input type="submit" className="btn btn-primary" id="submitButton" value="Submit"/>   
             </form>
         </div>
